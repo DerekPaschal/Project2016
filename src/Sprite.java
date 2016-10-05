160f4ba36a37c329050af50ce309189ff3ebb2ed
Derek Paschal <dtptoss@gmail.com>
2016-10-05 13:51:12 -0500

    Sprite code

---
+++ src/Sprite.java
@@ -0,0 +1,9 @@
+import java.awt.Graphics2D;
+
+public class Sprite 
+{
+	void draw(Graphics2D g2)
+	{
+		
+	}
+}
--- src/SpriteView.java
+++ src/SpriteView.java
@@ -1,5 +1,43 @@
+import java.util.LinkedList;
 
-public class SpriteView 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class SpriteView extends JFrame
 {
+	private SpritePanel panel;
+	public LinkedList<Sprite> SpriteList;
+	
+	public SpriteView(String WindowTitle, LinkedList<Sprite> newSpriteList)
+	{
+		this.setTitle(WindowTitle);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//this.setUndecorated(true);
+		this.setVisible(true);
+		
+		this.panel = new SpritePanel(this.getWidth(), this.getHeight(), this.SpriteList);
+		this.getContentPane().add(this.panel);
+		this.setIgnoreRepaint(true);
+	}
+	
+	public void redraw()
+	{
+		this.repaint(0);
+	}
+}
 
+
+
+@SuppressWarnings("serial")
+class SpritePanel extends JPanel
+{
+	private int windowX, windowY;
+	private LinkedList<Sprite> SpriteList;
+	
+	SpritePanel(int windowX, int windowY, LinkedList<Sprite> newSpriteList)
+	{
+		
+	}
 }
