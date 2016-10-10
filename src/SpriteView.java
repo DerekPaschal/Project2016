fb9da060a883ad606e7ee11f977ba3d8fdab85b4
Derek Paschal <dtptoss@gmail.com>
2016-10-10 17:35:29 -0500

    Fix SpriteView for Future Use
    Fix incompatablilty between SpriteView and main program.

--- src/SpriteView.java
+++ src/SpriteView.java
@@ -12,57 +12,16 @@ import javax.swing.JPanel;
  * @author Derek Paschal
  *
  * The purpose of SpriteView is to create a display a window that is the size of the current
- * display and iterate through a Sprite list that draws to the display.
+ * display and iterate through Sprite lists that draw to the display.
  */
 @SuppressWarnings("serial")
-public class SpriteView extends JFrame
-{
-	private SpritePanel panel;
-	
-	/**
-	 * 
-	 * @param WindowTitle The title of the window that will be created.
-	 * @param ForeGroundSprites The Sprite list that will be allowed to draw to the foreground each frame.
-	 * @param BackGroundSprites The Sprite list that will be allowed to draw to the background each frame.
-	 */
-	public SpriteView(String WindowTitle, ArrayList<Sprite> ForeGroundSprites, ArrayList<Sprite> BackGroundSprites, ViewCamera camera)
-	{
-		this.setTitle(WindowTitle);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		//this.setUndecorated(true);
-		this.setVisible(true);
-		
-		camera.Zoom = 1.0;
-		camera.windowDim = new Vector2D(this.getWidth(), this.getHeight());
-		camera.pos = new Vector2D(camera.windowDim.x/2.0, camera.windowDim.y/2.0);
-		
-		this.panel = new SpritePanel(camera, ForeGroundSprites, BackGroundSprites);
-		this.getContentPane().add(this.panel);
-		this.setIgnoreRepaint(true);
-	}
-	
-	public void redraw()
-	{
-		this.repaint(0);
-	}
-	
-	public void redraw(Vector2D cameraPosition, double scale)
-	{
-		this.repaint(0);
-	}
-}
-
-
-
-@SuppressWarnings("serial")
-class SpritePanel extends JPanel
+public class SpriteView extends JPanel
 {
 	ViewCamera camera;
 	private ArrayList<Sprite> ForeGroundSprites;
 	private ArrayList<Sprite> BackGroundSprites;
 	
-	SpritePanel(ViewCamera camera, ArrayList<Sprite> ForeGroundSprites, ArrayList<Sprite> BackGroundSprites)
+	SpriteView(ViewCamera camera, ArrayList<Sprite> ForeGroundSprites, ArrayList<Sprite> BackGroundSprites)
 	{
 		this.camera = camera;
 		this.ForeGroundSprites = ForeGroundSprites;
@@ -90,3 +49,42 @@ class SpritePanel extends JPanel
 		}
 	}
 }
+
+
+
+
+
+
+/*@SuppressWarnings("serial")
+public class SpriteView extends JFrame
+{
+	private SpritePanel panel;
+	
+	*//**
+	 * 
+	 * @param WindowTitle The title of the window that will be created.
+	 * @param ForeGroundSprites The Sprite list that will be allowed to draw to the foreground each frame.
+	 * @param BackGroundSprites The Sprite list that will be allowed to draw to the background each frame.
+	 *//*
+	public SpriteView(String WindowTitle, ArrayList<Sprite> ForeGroundSprites, ArrayList<Sprite> BackGroundSprites, ViewCamera camera)
+	{
+		this.setTitle(WindowTitle);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//this.setUndecorated(true);
+		this.setVisible(true);
+		
+		camera.Zoom = 1.0;
+		camera.windowDim = new Vector2D(this.getWidth(), this.getHeight());
+		camera.pos = new Vector2D(camera.windowDim.x/2.0, camera.windowDim.y/2.0);
+		
+		this.panel = new SpritePanel(camera, ForeGroundSprites, BackGroundSprites);
+		this.getContentPane().add(this.panel);
+		this.setIgnoreRepaint(true);
+	}
+	
+	public void redraw()
+	{
+		this.repaint(0);
+	}
+}*/
